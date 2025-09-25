1664ccaee12324acde4054f2731453f90062b7be
Noy Itzikowitz <nitzikow@redhat.com>
2025-09-25 11:01:33 -0700

    revert changes around Response Token struct

--- maas-api/internal/token/handler_types.go
+++ maas-api/internal/token/handler_types.go
@@ -1,9 +1,12 @@
 package token
 
 type Request struct {
-	Expiration *Duration `json:"expiration,omitempty"` // Expiration duration object
+	// Accepts either:
+	// - String: Go-style duration starting from seconds (e.g. `"30s"`, `"2h45m"`)
+	// - Number: Seconds (e.g. `3600`)
+	Expiration *Duration `json:"expiration,omitempty"`
 }
 
 type Response struct {
-	*Token `json:",inline"`
+	*Token `json:",inline,omitempty"`
 }
